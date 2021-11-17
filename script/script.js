0c6ba8dae11115fbcca2a6f7fecb6b538c322dc5
Evgeny Andryushchenko <evgeniy.andryushchenko@gmail.com>
2021-11-17 19:40:20 +0300

    fix: исправить наименование констант

--- script/script.js
+++ script/script.js
@@ -1,41 +1,41 @@
-const PROFILE = document.querySelector('.profile');
-const NAME_PROFILE = PROFILE.querySelector('.profile__name');
-const JOB_PROFILE = PROFILE.querySelector('.profile__profession');
-const EDIT_BUTTON_PROFILE = PROFILE.querySelector('.profile__edit-button');
+const profile = document.querySelector('.profile');
+const nameProfile = profile.querySelector('.profile__name');
+const jobProfile = profile.querySelector('.profile__profession');
+const editButtonProfile = profile.querySelector('.profile__edit-button');
 
-const POPUP = document.querySelector('.popup');
-const NAME_INPUT = POPUP.querySelector('.popup__input_type_name');
-const JOB_INPUT = POPUP.querySelector('.popup__input_type_profession');
-const SUBMIT_BUTTON = POPUP.querySelector('.popup__button_type_submit');
-const CLOSE_BUTTON = POPUP.querySelector('.popup__button_type_close');
+const popup = document.querySelector('.popup');
+const nameInput = popup.querySelector('.popup__input_type_name');
+const jobInput = popup.querySelector('.popup__input_type_profession');
+const submitButton = popup.querySelector('.popup__button_type_submit');
+const closeButton = popup.querySelector('.popup__button_type_close');
 
 
 function openPopup() {
-  POPUP.classList.add('popup_visible');
+  popup.classList.add('popup_visible');
 }
 
 function closePopup() {
-  POPUP.classList.remove('popup_visible');
-  EDIT_BUTTON_PROFILE.addEventListener('click', handleEditProfile);
+  popup.classList.remove('popup_visible');
+  editButtonProfile.addEventListener('click', handleEditProfile);
 }
 
 function submitPopup() {
-  NAME_PROFILE.textContent = NAME_INPUT.value;
-  JOB_PROFILE.textContent = JOB_INPUT.value;
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
 
   closePopup();
 }
 
 function handleEditProfile(evt) {
   evt.preventDefault();
-  NAME_INPUT.value = NAME_PROFILE.textContent;
-  JOB_INPUT.value = JOB_PROFILE.textContent;
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
 
-  CLOSE_BUTTON.addEventListener('click', closePopup);
-  SUBMIT_BUTTON.addEventListener('click', submitPopup);
+  closeButton.addEventListener('click', closePopup);
+  submitButton.addEventListener('click', submitPopup);
 
   openPopup()
 }
 
 
-EDIT_BUTTON_PROFILE.addEventListener('click', handleEditProfile);
+editButtonProfile.addEventListener('click', handleEditProfile);
