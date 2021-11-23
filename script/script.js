f52513eae89ed688240d5c0f58d5f04663be6ef6
Андрющенко Евгений Николаевич <17106010@sigma.sbrf.ru>
2021-11-24 00:12:35 +0300

    feat: добавить шаблон

--- script/script.js
+++ script/script.js
@@ -10,6 +10,7 @@ const submitButton = popup.querySelector('.popup__button_type_submit');
 const closeButton = popup.querySelector('.popup__button_type_close');
 
 
+
 function openPopup() {
   popup.classList.add('popup_visible');
 }
@@ -39,3 +40,48 @@ function handleEditProfile() {
 
 
 editButtonProfile.addEventListener('click', handleEditProfile);
+
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+
+const photoContainer = document.querySelector('.photo-grid__items');
+
+function createPhotoElement(...placeName, ...photoLink) {
+  const templatePhoto = document.querySelector('#template-photo').content;
+  const photoItem = templatePhoto.querySelector('.photo-grid__item').cloneNode(true);
+  photoItem.querySelector('.photo-grid__title').textContent = placeName;
+  photoItem.querySelector('.photo-grid__image').src = photoLink;
+  photoItem.querySelector('.photo-grid__image').alt = placeName;
+
+  photoContainer.append(photoItem);
+}
+
+createPhotoElement(...initialCards.name, ...initialCards.link);
+createPhotoElement(initialCards[1].name, initialCards[1].link);
