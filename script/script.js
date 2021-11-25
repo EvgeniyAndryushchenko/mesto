0e43780b4eab7acfcf672a80b291e923a30ee551
Evgeniy <evgeniy.andryushchenko@gmail.com>
2021-11-26 00:34:31 +0300

    feat: добавить шаблон попапа из js

--- script/script.js
+++ script/script.js
@@ -2,37 +2,35 @@ const profile = document.querySelector('.profile');
 const nameProfile = profile.querySelector('.profile__name');
 const jobProfile = profile.querySelector('.profile__profession');
 const editButtonProfile = profile.querySelector('.profile__edit-button');
+const addButtonProfile = profile.querySelector('.profile__add-button');
 
+
+const submitButtons = document.querySelectorAll('.popup__button_type_submit');
+const closeButtons = document.querySelectorAll('.popup__button_type_close');
 const popup = document.querySelector('.popup');
-const nameInput = popup.querySelector('.popup__input_type_name');
-const jobInput = popup.querySelector('.popup__input_type_profession');
-const submitButton = popup.querySelector('.popup__button_type_submit');
-const closeButton = popup.querySelector('.popup__button_type_close');
 
-
+const initialPopups = [
+  {
+    popupClass: 'popup__profile',
+    nameId: 'popup__input_type_name',
+    additionId: 'popup__input_type_profession',
+    heading: 'Редактировать профиль',
+    name: 'Жак-Ив Кусто',
+    addition: 'Исследователь океана'
+  },
+  {
+    popupClass: 'popup__picture',
+    nameId: 'popup__input_type_heading',
+    additionId: 'popup__input_type_link',
+    heading: 'Новое место',
+    name: 'Название',
+    addition: 'Ссылка на картинку'
+  }
+];
 
 function openPopup() {
   popup.classList.add('popup_visible');
 }
-
-function createPopup(heading, name, profession) {
-  const templatePopup = document.querySelector('#template-popup').content;
-  const popup = templatePopup.querySelector('.popup').cloneNode(true);
-  popup.querySelector('.popup__heading').textContent = heading;
-  popup.querySelector('.popup__input_type_name').placeholder = name;
-  popup.querySelector('.popup__input_type_profession').placeholder = profession;
-
-
-  popup.classList.add('popup_visible');
-  return popup;
-}
-
-document.querySelector('.page').append(createPopup("Редактировать профиль", "Жак-Ив", "Исследователь"));
-
-//let createListPhoto = initialCards.map(function (item) {
-//  return createPhotoElement(item);
-//});
-
 
 function closePopup() {
   popup.classList.remove('popup_visible');
@@ -41,24 +39,51 @@ function closePopup() {
 
 function submitPopup(evt) {
   evt.preventDefault();
-  nameProfile.textContent = nameInput.value;
-  jobProfile.textContent = jobInput.value;
+  //nameProfile.textContent = nameInput.value;
+  //jobProfile.textContent = jobInput.value;
 
   closePopup();
 }
 
-function handleEditProfile() {
-  nameInput.value = nameProfile.textContent;
-  jobInput.value = jobProfile.textContent;
+function createPopup(item) {
+  const templateObject = document.querySelector('#template-popup').content;
+  const templatePopup = templateObject.querySelector('.popup').cloneNode(true);
+  const submitButton = templatePopup.querySelector('.popup__button_type_submit');
+  const closeButton = templatePopup.querySelector('.popup__button_type_close');
+  const nameInput = templatePopup.querySelector('.popup__input_type_name');
+  const additionInput = templatePopup.querySelector('.popup__input_type_addition');
 
+  templatePopup.querySelector('.popup__heading').textContent = item.heading;
+  nameInput.id = item.nameId;
+  nameInput.placeholder = item.name;
+  additionInput.id = item.additionId;
+  additionInput.placeholder = item.addition;
+
+  templatePopup.classList.add(item.popupClass);
   closeButton.addEventListener('click', closePopup);
   submitButton.addEventListener('click', submitPopup);
+  return templatePopup;
+}
+
+let createListPopups = initialPopups.map(function (item) {
+  return createPopup(item);
+});
+
+document.querySelector('.content').append(...createListPopups);
+
+function handleEditProfile() {
+  //nameInput.value = nameProfile.textContent;
+  //jobInput.value = jobProfile.textContent;
+
+  //closeButtons.addEventListener('click', closePopup);
+  //submitButtons.addEventListener('click', submitPopup);
 
   openPopup()
 }
 
 
 editButtonProfile.addEventListener('click', handleEditProfile);
+addButtonProfile.addEventListener('click', handleEditProfile);
 
 
 
